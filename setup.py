3cfb9e6a9401205708f0f04e88114802ccc54d27
Wolfgang Maier <wolfgang.maier@gmail.com>
2014-03-20 13:38:06 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ if sys.version < '2.7.3':
    print 'Requires Python version >= 2.7.3'
    sys.exit(1)
 setup(name='treetools',
-      version='0.0.1', # major.minor.revision
+      version='0.1.0', # major.minor.revision
       description='Tools for processing treebank trees',
       author='Wolfgang Maier',
       author_email='wolfgang.maier@gmail.com',
