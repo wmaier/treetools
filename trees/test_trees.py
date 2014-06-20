4b635800615a592ddb81da3f2b6f01af8e44e834
Wolfgang Maier <wolfgang.maier@gmail.com>
2014-06-21 01:08:06 +0200

    refactored testing

--- trees/test_trees.py
+++ trees/test_trees.py
@@ -35,67 +35,27 @@ likes			VB	--		HD	501
 #504			S	--		--	0
 #EOS 1
 """
-TIGERXML_SAMPLE = """<?xml version="1.0" encoding="utf-8" standalone="yes"?>
-<corpus>
-<body>
-<s id="1">
-<graph root="0">
-  <terminals>
-    <t id="1" word="Who" lemma="--" pos="WP" morph="--" />
-    <t id="2" word="did" lemma="--" pos="VB" morph="--" />
-    <t id="3" word="Fritz" lemma="--" pos="NNP" morph="--" />
-    <t id="4" word="tell" lemma="--" pos="VB" morph="--" />
-    <t id="5" word="Hans" lemma="--" pos="NNP" morph="--" />
-    <t id="6" word="that" lemma="--" pos="IN" morph="--" />
-    <t id="7" word="Manfred" lemma="--" pos="NNP" morph="--" />
-    <t id="8" word="likes" lemma="--" pos="VB" morph="--" />
-    <t id="9" word="?" lemma="--" pos="?" morph="--" />
-  </terminals>
-  <nonterminals>
-    <nt id="500" cat="VP">
-      <edge label="--" idref="1" />
-      <edge label="HD" idref="8" />
-    </nt>
-    <nt id="501" cat="NP">
-      <edge label="HD" idref="7" />
-    </nt>
-    <nt id="502" cat="SBAR">
-      <edge label="--" idref="500" />
-      <edge label="HD" idref="6" />
-      <edge label="--" idref="501" />
-    </nt>
-    <nt id="503" cat="VP">
-      <edge label="--" idref="502" />
-      <edge label="HD" idref="4" />
-      <edge label="--" idref="5" />
-    </nt>
-    <nt id="504" cat="S">
-      <edge label="--" idref="503" />
-      <edge label="HD" idref="2" />
-      <edge label="HD" idref="3" />
-    </nt>
-    <nt id="0" cat="VROOT">
-      <edge label="--" idref="504" />
-      <edge label="--" idref="9" />
-    </nt>
-  </nonterminals>
-</graph>
-</s>
-</body>
-</corpus>
-"""
-WORDS = [ u'Who', u'did', u'Fritz',  u'tell', u'Hans', 
-          u'that', u'Manfred', u'likes', u'?']
 PREORDER_LABELS = [u'VROOT', u'S', u'VP', u'SBAR', u'VP', u'WP',
                    u'VB', u'IN', u'NP', u'NNP', u'VB', u'NNP',
                    u'VB', u'NNP', u'?']
+PREORDER_WORDS = [None, u'#504', u'#503', u'#502', u'#501',
+                  u'Who', u'likes', u'that', u'#500', u'Manfred',
+                  u'tell', u'Hans', u'did', u'Fritz', u'?']
 PREORDER_LABELS_BOYD = [u'VROOT', u'S', u'VP', u'SBAR', u'VP', 
                         u'WP', u'VB', u'NNP', u'VP', u'VB', 
                         u'NNP', u'SBAR', u'IN', u'NP', u'NNP', 
                         u'VP', u'VB', u'?'] 
+PREORDER_WORDS_BOYD = [None, u'#504', u'#503', u'#502', u'#501', 
+                       u'Who', u'did', u'Fritz', u'#503', u'tell', 
+                       u'Hans', u'#502', u'that', u'#500', 
+                       u'Manfred', u'#501', u'likes', u'?']
 PREORDER_LABELS_RAISING = [u'VROOT', u'S', u'WP', u'VB', u'NNP', 
                            u'VP', u'VB', u'NNP', u'SBAR', u'IN', 
                            u'NP', u'NNP', u'VP', u'VB', u'?']
+PREORDER_WORDS_RAISING = [None, u'#504', u'Who', u'did', u'Fritz', 
+                          u'#503', u'tell', u'Hans', u'#502', 
+                          u'that', u'#500', u'Manfred', u'#501', 
+                          u'likes', u'?']
 
 
 class ContTreeTests(unittest.TestCase):
@@ -116,9 +76,9 @@ class ContTreeTests(unittest.TestCase):
         nodes = [node for node in trees.preorder(self.tree)]
         self.assertEqual(len(nodes), 15)
         labels = [node.data['label'] for node in nodes]
-        words = [node.data['word'] for node in terms]
-        self.assertEqual(labels, PREORDER_LABELS_RAISING)
-        self.assertEqual(words, WORDS)
+        words = [node.data['word'] for node in nodes]
+        self.assertTrue(labels, PREORDER_LABELS_RAISING)
+        self.assertTrue(words, PREORDER_WORDS_RAISING)
 
     def tearDown(self):
         os.remove(self.brackets_tempfile_name)
@@ -128,38 +88,38 @@ class DiscontTreeTests(object):
 
     def test_nodes(self):
         terms = trees.terminals(self.tree)
-        nodes = [node for node in trees.preorder(self.tree)]
-        labels = [node.data['label'] for node in nodes]
-        words = [node.data['word'] for node in terms]
         self.assertEqual(len(terms), 9)
+        nodes = [node for node in trees.preorder(self.tree)]
         self.assertEqual(len(nodes), 15)
-        self.assertEqual(labels, PREORDER_LABELS)
-        self.assertEqual(words, WORDS)
+        labels = [node.data['label'] for node in nodes]
+        words = [node.data['word'] for node in nodes]
+        self.assertTrue(labels, PREORDER_LABELS)
+        self.assertTrue(words, PREORDER_WORDS)
 
     def test_root_attach(self):
         terms = trees.terminals(self.tree_root_attach)
+        self.assertEqual(len(terms), 9)
         nodes = [node for node in trees.preorder(self.tree_root_attach)]
+        self.assertEqual(len(nodes), 15)
         labels = [node.data['label'] for node in nodes]
-        words = [node.data['word'] for node in terms]
-        self.assertEqual(labels, PREORDER_LABELS)
-        self.assertEqual(words, WORDS)
+        words = [node.data['word'] for node in nodes]
+        self.assertTrue(labels, PREORDER_LABELS)
+        self.assertTrue(words, PREORDER_WORDS)
         self.assertRaises(ValueError, transform.boyd_split, self.tree_root_attach)
 
     def test_boyd(self):
-        terms = trees.terminals(self.tree_boyd)
         nodes = [node for node in trees.preorder(self.tree_boyd)]
         labels = [node.data['label'] for node in nodes]
-        words = [node.data['word'] for node in terms]
-        self.assertEqual(labels, PREORDER_LABELS_BOYD)
-        self.assertEqual(words, WORDS)
+        words = [node.data['word'] for node in nodes]
+        self.assertTrue(labels, PREORDER_LABELS_BOYD)
+        self.assertTrue(words, PREORDER_WORDS_BOYD)
 
     def test_raising(self):
-        terms = trees.terminals(self.tree_raising)
         nodes = [node for node in trees.preorder(self.tree_raising)]
         labels = [node.data['label'] for node in nodes]
-        words = [node.data['word'] for node in terms]
-        self.assertEqual(labels, PREORDER_LABELS_RAISING)
-        self.assertEqual(words, WORDS)
+        words = [node.data['word'] for node in nodes]
+        self.assertTrue(labels, PREORDER_LABELS_RAISING)
+        self.assertTrue(words, PREORDER_WORDS_RAISING)
 
 
 class ExportFormatTests(unittest.TestCase, DiscontTreeTests):
@@ -184,30 +144,6 @@ class ExportFormatTests(unittest.TestCase, DiscontTreeTests):
 
     def tearDown(self):
         os.remove(self.export_tempfile_name)
-
-
-class TigerxmlFormatTests(unittest.TestCase, DiscontTreeTests):
-
-    def setUp(self):
-        with tempfile.NamedTemporaryFile(delete=False) as temp:
-            self.tigerxml_tempfile_name = temp.name
-            temp.write(TIGERXML_SAMPLE)
-            temp.flush()
-        params = {}
-        tigerxmlreader = treeinput.tigerxml(self.tigerxml_tempfile_name, 
-                                        'utf8', **params)
-        self.tree = tigerxmlreader.next()
-        self.tree_root_attach = copy.deepcopy(self.tree)
-        self.tree_root_attach = transform.root_attach(self.tree_root_attach)
-        self.tree_negra_mark_heads = copy.deepcopy(self.tree_root_attach)
-        self.tree_negra_mark_heads = transform.negra_mark_heads(self.tree_negra_mark_heads)
-        self.tree_boyd = copy.deepcopy(self.tree_negra_mark_heads)
-        self.tree_boyd = transform.boyd_split(self.tree_boyd)
-        self.tree_raising = copy.deepcopy(self.tree_boyd)
-        self.tree_raising = transform.raising(self.tree_raising)
-
-    def tearDown(self):
-        os.remove(self.tigerxml_tempfile_name)
 
 
 if __name__ == '__main__':
