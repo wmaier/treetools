d6571de86324a2c9e074ee92a4ddc22fc7b039e9
Wolfgang Maier <wolfgang.maier@gmail.com>
2017-09-21 21:55:05 +0200

    removed output options

--- trees/transitionoutput.py
+++ trees/transitionoutput.py
@@ -12,10 +12,10 @@ import io
 def plain(trans, dest, dest_enc, **params):
     """Write plain transitions.
     """
-    with io.open("%s" % dest, 'w', encoding=dest_enc) as trans_stream:
+    with io.open(u"%s" % dest, 'w', encoding=dest_enc) as trans_stream:
         for seq in trans:
-            trans_stream.write(u"%s\n" % ' '.join(["%s" % t.pretty_print() for t in seq]))
+            trans_stream.write(u"%s\n" % ' '.join([u"%s" % t.pretty_print() for t in seq]))
 
 
 FORMATS = [plain]
-FORMAT_OPTIONS = {'sentences': 'output sentences with transitions.'}
+FORMAT_OPTIONS = {}
