ae2b0e567f0678e85c42f67f251702303eacfb69
Wolfgang Maier <wolfgang.maier@gmail.com>
2020-02-08 13:45:29 -0400

    fix tests

--- tests/testdata_transitions.py
+++ tests/testdata_transitions.py
@@ -10,5 +10,5 @@ TRANS_TERMINALS = [('Who', 'WP'), ('did', 'VB'), ('Fritz', 'NNP'), (
     'tell', 'VB'), ('Hans', 'NNP'), ('that', 'IN'), ('Manfred', 'NNP'), ('likes', 'VB'), ('?', '?')]
 TRANS_CONT_TOPDOWN_NEGRAHEADS_TRANSITIONS = ["SHIFT", "SHIFT", "UNARY-VP", "SHIFT", "UNARY-NP", "SHIFT", "BINARY-LEFT-@SBAR", "BINARY-LEFT-SBAR", "SHIFT",
                                              "SHIFT", "BINARY-LEFT-@VP", "BINARY-LEFT-VP", "SHIFT", "SHIFT", "SHIFT", "BINARY-LEFT-@S", "BINARY-LEFT-@S", "BINARY-LEFT-S", "BINARY-LEFT-VROOT"]
-TRANS_DISCONT_GAP_TRANSITIONS = ["SHIFT", "SHIFT", "SHIFT", "RLEFT-@S", "SHIFT", "SHIFT", "RLEFT-@VP", "SHIFT", "SHIFT", "UNARY-NP",
-                                 "RLEFT-@SBAR", "SHIFT", "GAP", "GAP", "GAP", "RRIGHT-VP", "GAP", "GAP", "RLEFT-SBAR", "RLEFT-VP", "RLEFT-S", "SHIFT", "RLEFT-VROOT"]
+TRANS_DISCONT_GAP_TRANSITIONS = ["SHIFT", "SHIFT", "SHIFT", "R-LEFT-@S", "SHIFT", "SHIFT", "R-LEFT-@VP", "SHIFT", "SHIFT", "UNARY-NP",
+                                 "R-LEFT-@SBAR", "SHIFT", "GAP", "GAP", "GAP", "R-RIGHT-VP", "GAP", "GAP", "R-LEFT-SBAR", "R-LEFT-VP", "R-LEFT-S", "SHIFT", "R-LEFT-VROOT"]
